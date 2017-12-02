687843fc06b899d93d8ddfcb6165ee2f95da629f
WX <paep3nguin@gmail.com>
2017-12-02 11:44:03 -0800

    Solution for day 2, part 2

--- 2/2.py
+++ 2/2.py
@@ -3,12 +3,30 @@
 
 def main():
     with open('2.txt', 'r') as f, open('2_out.txt', 'w') as f_out:
+        grid = [[int(n) for n in line.split()] for line in f.readlines()]
+        # Part 1
         checksum = 0
-        for line in f:
-            numbers = [int(n) for n in line.split()]
-            checksum += max(numbers) - min(numbers)
+        for row in grid:
+            checksum += max(row) - min(row)
         print(checksum)
-        f_out.write(str(checksum))
+        print(checksum, file=f_out)
+
+        # Part 2
+        div_sum = 0
+        for row in grid:
+            div_sum += find_quotient(row)
+        print(div_sum)
+        print(div_sum, file=f_out)
+
+
+def find_quotient(row):
+    num_set = set(row)
+    largest = max(row)
+    for n in row[:-1]:
+        max_multiple = int(largest / n)
+        for m in range(2, max_multiple + 1):
+            if n * m in num_set:
+                return m
 
 
 if __name__ == '__main__':
