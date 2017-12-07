5facc1dd065c410be5e5bf7b1764601e1872020e
WX <paep3nguin@gmail.com>
2017-12-06 23:32:00 -0800

    Condense solutions for day 6 into one loop

--- 6/6.py
+++ 6/6.py
@@ -3,33 +3,7 @@
 
 def main():
     with open('6.txt', 'r') as f, open('6_out.txt', 'w') as f_out:
-        og_banks = [int(n) for n in f.readline().split()]
-
-        # Part 1
-        banks = og_banks.copy()
-        num_banks = len(banks)
-        seen = {tuple(banks)}
-
-        steps = 0
-        while True:
-            steps += 1
-            m, i = list_max(banks)
-            banks[i] = 0
-
-            for j in range(i + 1, i + m + 1):
-                banks[j % num_banks] += 1
-
-            t = tuple(banks)
-            if t not in seen:
-                seen.add(t)
-            else:
-                break
-
-        print(steps)
-        print(steps, file=f_out)
-
-        # Part 2
-        banks = og_banks.copy()
+        banks = [int(n) for n in f.readline().split()]
         num_banks = len(banks)
         seen = {tuple(banks): 0}
 
@@ -46,6 +20,11 @@ def main():
             if t not in seen:
                 seen[t] = steps
             else:
+                # Part 1
+                print(steps)
+                print(steps, file=f_out)
+
+                # Part 2
                 loop_len = steps - seen[t]
                 print(loop_len)
                 print(loop_len, file=f_out)
