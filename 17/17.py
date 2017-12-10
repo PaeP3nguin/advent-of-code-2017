ee44f2b7752e1ab101f53d46d656ad8b4e5003f2
WX <paep3nguin@gmail.com>
2017-12-10 13:44:15 -0800

    Generated folders and template for all future days

---
+++ 17/17.py
@@ -0,0 +1,19 @@
+#!python3
+
+
+def main():
+    with open('17.txt', 'r') as f, open('17_out.txt', 'w') as f_out:
+        lines = f.readlines()
+
+        # Part 1
+        answer = 0
+        print(answer)
+        print(answer, file=f_out)
+
+        # Part 2
+        print(answer)
+        print(answer, file=f_out)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 20/20.py
@@ -0,0 +1,19 @@
+#!python3
+
+
+def main():
+    with open('20.txt', 'r') as f, open('20_out.txt', 'w') as f_out:
+        lines = f.readlines()
+
+        # Part 1
+        answer = 0
+        print(answer)
+        print(answer, file=f_out)
+
+        # Part 2
+        print(answer)
+        print(answer, file=f_out)
+
+
+if __name__ == '__main__':
+    main()
