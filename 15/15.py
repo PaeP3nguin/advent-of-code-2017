2d0c648475d7ca3dbd98de9c0b1aba4c2d28b6bd
WX <paep3nguin@gmail.com>
2017-12-16 23:21:25 -0800

    Solutions for day 15

--- 15/15.py
+++ 15/15.py
@@ -1,18 +1,53 @@
 #!python3
 
+from tqdm import tqdm
+
 
 def main():
     with open('15.txt', 'r') as f, open('15_out.txt', 'w') as f_out:
-        lines = f.readlines()
+        a_line, bee_line = f.readlines()
+        a_first = int(a_line.strip().split()[-1])
+        b_first = int(bee_line.strip().split()[-1])
+
+        a_factor = 16807
+        b_factor = 48271
+        dividend = 2147483647
 
         # Part 1
-        answer = 0
-        print(answer)
-        print(answer, file=f_out)
+        matches = 0
+        gen_a = generator(a_first, a_factor, dividend)
+        gen_b = generator(b_first, b_factor, dividend)
+        for i in tqdm(range(40 * 1000 * 1000)):
+            a_val = next(gen_a)
+            b_val = next(gen_b)
+            if '{:0>16b}'.format(a_val)[-16:] == '{:0>16b}'.format(b_val)[-16:]:
+                matches += 1
+        print(matches)
+        print(matches, file=f_out)
+
+        a_criteria = 4
+        b_criteria = 8
 
         # Part 2
-        print(answer)
-        print(answer, file=f_out)
+        matches = 0
+        gen_a = generator(a_first, a_factor, dividend, a_criteria)
+        gen_b = generator(b_first, b_factor, dividend, b_criteria)
+        for i in tqdm(range(5 * 1000 * 1000)):
+            a_val = next(gen_a)
+            b_val = next(gen_b)
+            if '{:0>16b}'.format(a_val)[-16:] == '{:0>16b}'.format(b_val)[-16:]:
+                matches += 1
+        print(matches)
+        print(matches, file=f_out)
+
+
+def generator(init, factor, dividend, criteria=1):
+    last = init
+    while True:
+        curr = (last * factor) % dividend
+        if curr % criteria == 0:
+            yield curr
+        last = curr
 
 
 if __name__ == '__main__':
