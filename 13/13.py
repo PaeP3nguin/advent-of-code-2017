36c35be9562c07bec983bd0675e02e6ec0a7abfb
WX <paep3nguin@gmail.com>
2017-12-16 18:53:38 -0800

    Solutions for day 13

--- 13/13.py
+++ 13/13.py
@@ -5,14 +5,35 @@ def main():
     with open('13.txt', 'r') as f, open('13_out.txt', 'w') as f_out:
         lines = f.readlines()
 
+        depths = {}
+        for line in lines:
+            layer, depth = [int(n) for n in line.strip().split(': ')]
+            depths[layer] = depth
+        # depths = {0:3, 1:2, 4:4, 6:4}
+        max_depth = max(depths.keys())
+
         # Part 1
-        answer = 0
-        print(answer)
-        print(answer, file=f_out)
+        severity = 0
+        for depth, r in depths.items():
+            if (depth % ((r - 1) * 2)) == 0:
+                severity += depth * r
+        print(severity)
+        print(severity, file=f_out)
 
         # Part 2
-        print(answer)
-        print(answer, file=f_out)
+        delay = 0
+        while True:
+            caught = False
+            for depth, r in depths.items():
+                if ((delay + depth) % ((r - 1) * 2)) == 0:
+                    caught = True
+                    break
+            if not caught:
+                break
+            else:
+                delay += 1
+        print(delay)
+        print(delay, file=f_out)
 
 
 if __name__ == '__main__':
